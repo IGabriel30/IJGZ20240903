ac1bfc03e96577762798b486fff6aae5a95e69b0
IGabriel30 <ingridgabriel30@hotmail.com>
2024-09-03 11:39:08 -0600

    Se crea modelo Alumno

---
+++ IJGZ20240903/Models/Alumno.cs
@@ -0,0 +1,11 @@
+﻿namespace IJGZ20240903.Models
+{
+    public class Alumno
+    {
+       public int Id { get; set; }
+       public string Nombe { get; set; }
+       public string Apellido { get; set; }
+       public string Direccion { get; set; }
+        
+    }
+}
